6302f5c1501b6f605a65abd53bda1596a680eb70
Parvez M Robin <parvezmrobin@gmail.com>
2022-11-28 19:23:49 -0400

    log to file only if the file-system is writable

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -7,13 +7,26 @@
 const winston = require('winston');
 require('winston-daily-rotate-file');
 
-const {simple, colorize} = winston.format;
+const { simple, colorize } = winston.format;
 const axios = require('axios');
+const fs = require('fs');
+
+let writable = false;
+try {
+  fs.accessSync('text.txt', fs.constants.W_OK);
+  writable = true;
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.error(err);
+}
 
 const logger = winston.createLogger({
   level: 'info',
   format: simple(),
-  transports: [
+});
+
+if (writable) {
+  const fileTransports = [
     //
     // - Write to all logs with level `info` and below to `combined.log`
     // - Write to all logs with level `warn` and below to `validation.log`
@@ -33,14 +46,18 @@ const logger = winston.createLogger({
       filename: 'logs/%DATE%/combined.log',
       datePattern: 'YYYY-MM-DD',
     }),
-  ],
-});
+  ];
+
+  for (const fileTransport of fileTransports) {
+    logger.add(fileTransport);
+  }
+}
 
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-if (process.env.NODE_ENV !== 'production') {
+if (!writable || process.env.NODE_ENV !== 'production') {
   const consoleTransport = new winston.transports.Console({
-    format: colorize({all: true}),
+    format: colorize({ all: true }),
   });
   logger.add(consoleTransport);
 }
@@ -49,15 +66,21 @@ async function amplitude(eventName, userId, data, time) {
   // eslint-disable-next-line no-param-reassign
   time = Number(time || new Date());
   try {
-    await axios
-      .post('https://api.amplitude.com/2/httpapi', {
-        api_key: process.env.AMPLITUDE_KEY,
-        events: [{
-          event_type: eventName, user_id: userId, event_properties: data, time,
-        }],
-      });
+    await axios.post('https://api.amplitude.com/2/httpapi', {
+      api_key: process.env.AMPLITUDE_KEY,
+      events: [
+        {
+          event_type: eventName,
+          user_id: userId,
+          event_properties: data,
+          time,
+        },
+      ],
+    });
   } catch (err) {
-    logger.error('Error Amplitude:', {err: err.response && err.response.data});
+    logger.error('Error Amplitude:', {
+      err: err.response && err.response.data,
+    });
   }
 }
 
